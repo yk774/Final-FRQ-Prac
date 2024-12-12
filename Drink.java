3052f1cc5868662295b22c2e42871bf905cce3b0
bryan thai <bthai@pleasantonusd.net>
2024-12-12 11:03:48 -0800

    first commit

---
+++ Drink.java
@@ -0,0 +1,32 @@
+public class Drink{
+    //Create attributes for the drink class here.
+      
+    /* Give it attirbutes:
+    name
+    price with decimals
+    does it have ice or not
+    if it's empty
+    */
+    
+    
+    
+    //Write the constructor for the Drink class. (THIS IS WHAT CREATES an object when its run in the main) 
+      
+    //After creating a constructor test your code in the main to see if you can create some drink.
+    
+    
+    
+    
+    
+      //Write getters/setters for the methods
+    
+    
+    
+    
+    
+    
+    
+    // Write the method emptyDrink which prints out tthe message: NameOfDrink "has been drank" and then changes the attribute to be empty
+    
+      
+    }
---
+++ Main.java
@@ -0,0 +1,43 @@
+class Main {
+    public static void main(String[] args) {
+  //Create 3 drink objects that cost less than $5
+  
+  //***TEST YOUR CODE BY PRINTING THE NAME OF YOUR DRINK TO MAKE SURE IT WORKS***//
+  
+  //Create 2 drink objects that cost more than $5
+  
+  
+  //create an array of drinks  with size 8.
+  //put all your drinks in that array.
+  
+  
+  
+      
+  //You will need to write 2 method below
+  //------------------------
+  //Test your method calls here.
+  
+  //test your cheapItems method
+  
+  
+  //test your longNameSale method
+  //Try to print the price before you use the method and after to see if it worked.
+  
+    }
+    
+  //Write a method cheapItems that will return go through an array of drinks and return a String that lists the number of items that cost less than $3
+  // For example " The cheap drinks are: Fountain drinks, Snapple, green Tea "
+  public static  String cheapItems(Drink[] arr){
+    //WRITE THE METHOD HERE. MAKE SURE YOU CHECK FOR Nulls 
+    return null; //replace this return statement when you write the code
+  }
+  
+  
+  //There is a sale on drinks with long names. Write a method that will change the price of all items with a name longer than 7 characters to $1.50; You will need to use a setter.
+  public static void longNameSale(Drink[] arr){
+  
+  
+  }
+    
+  
+  }
